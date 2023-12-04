6f261195462592161caeeb1c4d5c789d5f138ddb
Mauricio Alvarenga <mauricioalvarenga@Mauricios-MacBook-Pro.local>
2023-12-03 19:45:37 -0800

    completely mobile responsive, going to continue to work on other links but need to go and design some ui first to get an idea

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import classNames from "classnames";
 import vfwimage from "../assets/Images/vfw-1.jpeg";
-const Home = () => {
-  //Need state to store image data
-
+const Home = ({ isMobile }) => {
   const data = [
     {
       id: 1,
@@ -32,9 +31,10 @@ const Home = () => {
   return (
     <>
       <div className="w-full h-full ">
+        {/* Slider Container Start */}
         <div className="w-full h-1/3 flex justify-center pt-10">
           <div className="w-[90%] h-[250px]">
-            <Slider>
+            <Slider {...settings}>
               {data.map((image) => (
                 <>
                   <img
@@ -48,14 +48,29 @@ const Home = () => {
             </Slider>
           </div>
         </div>
+        {/* Slider Container End  */}
 
-        <div className="flex w-full h-full p-10">
-          <div className="w-1/2 h-full  ">
-            <div className = "w-full h-full ">
-              <h2 className="text-white text-3xl font-Rothwell p-10">
+        {/* TEXT-AREA-START */}
+
+        <div
+          className={classNames(
+            "w-full h-full p-10",
+            isMobile ? "flex flex-col" : "flex"
+          )}
+        >
+          <div
+            className={classNames("h-full  ", isMobile ? "w-full" : "w-1/2")}
+          >
+            <div className="w-full h-full ">
+              <h2
+                className={classNames(
+                  "text-white text-3xl font-Rothwell p-10",
+                  isMobile ? "text-2xl" : null
+                )}
+              >
                 Upcoming Events
               </h2>
-              <div className="leading-4 p-6 w-2/3">
+              <div className=" w-[90%] leading-4 p-6 ">
                 <p className="text-white text-sm">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -70,16 +85,31 @@ const Home = () => {
                   scelerisque purus semper eget duis at tellus at. Gravida arcu
                   ac tortor dignissim convallis.
                 </p>
+              </div>
+              <div className={classNames(" w-1/3 aspect-square rounded p-4 ", isMobile ? " w-11/12": null)}>
+                <iframe
+                  title = "Located"
+                  className="w-full h-full rounded"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3157.110012879915!2d-122.0875749241977!3d37.69361531683099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f9188123c6b5b%3A0x13bc47104e62b9a8!2sThe%20Moose%20-%20Hayward-Castro%20Valley%20Lodge%201491!5e0!3m2!1sen!2sus!4v1701658735475!5m2!1sen!2sus"
+xs                  loading="lazy"
+                ></iframe>
               </div>
             </div>
           </div>
           <div className="border-r border-2 border-white mr-12"></div>
-          <div className="w-1/2 h-full  ">
-            <div className = "w-full h-full ">
-              <h2 className="text-white text-3xl font-Rothwell p-10">
+          <div
+            className={classNames(" h-full  ", isMobile ? "w-full" : "w-1/2")}
+          >
+            <div className="w-full h-full ">
+              <h2
+                className={classNames(
+                  "text-white text-3xl font-Rothwell p-10",
+                  isMobile ? "text-2xl" : null
+                )}
+              >
                 Commanders Message
               </h2>
-              <div className="leading-4 p-6 w-2/3">
+              <div className="w-[80%] leading-4 p-6 ">
                 <p className="text-white text-sm">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -97,6 +127,7 @@ const Home = () => {
               </div>
             </div>
           </div>
+          {/* TEXT AREA END */}
         </div>
       </div>
     </>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="  h-[100px] sticky flex justify-evenly z-10  mx-auto">
+    <div className=" h-[100px] sticky flex justify-evenly z-10  mx-auto">
         
         <NavLink>
             <h2 className = "text-white  font-Libel text-2xl ">Events</h2>
